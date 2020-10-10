eab1c183e6e7f2f7ce59499a207f00de2e2a95d4
martin_dev <gmsuruguay@gmail.com>
2020-10-10 19:23:32 -0300

    se incluyo tabla en el dashboard

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-navbar-brand href="#">Dashboard</b-navbar-brand>
+
+            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+            <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+                <b-nav-item href="#">Contacto</b-nav-item>                
+            </b-navbar-nav>
+
+            
+            </b-collapse>
+        </b-navbar>
+    </div>
+</template>
+
+<script>   
+
+    export default {
+        name: 'Header'
+    }
+</script>
+
+<style scoped>
+
+</style>
