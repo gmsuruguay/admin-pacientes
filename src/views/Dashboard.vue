f49d88fff1cf0af6502e32a3dde23d2104eef811
martin_dev <gmsuruguay@gmail.com>
2020-10-17 13:05:56 -0300

    se muestra tabla

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,26 +1,51 @@
 <template>
   <div>
    <Header/>
-   <b-table striped hover :items="items"></b-table>
+   <table class="table">
+      <thead>
+        <tr>          
+          <th scope="col">Id</th>
+          <th scope="col">DNI</th>
+          <th scope="col">Nombre</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="paciente in listaPacientes" :key="paciente.PacienteId" @click="editar(paciente.PacienteId)">
+          <th scope="row">{{paciente.PacienteId}}</th>
+          <td>{{paciente.DNI}}</td>
+          <td>{{paciente.Nombre}}</td>          
+        </tr>       
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>  
  import Header from '@/components/Header.vue'
+ import axios from 'axios'
   export default {
     name: "Dashboard",
     components:{
       Header
     },
     data() {
-      return {
-        items: [
-          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
-          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
-          { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
-          { age: 38, first_name: 'Jami', last_name: 'Carney' }
-        ]
+      return {        
+        listaPacientes : null,
+        nroPag : 1              
       }
+    },
+    mounted(){
+      let nroPag = this.nroPag
+      let endPoint = "http://solodata.es/pacientes?page="+nroPag
+      axios.get(endPoint).then(result =>{
+        console.log(result.data)
+        this.listaPacientes = result.data
+      })
+    },
+    methods: {
+      editar(id) {
+        console.log(id)
+      },
     }
   }
 </script>
