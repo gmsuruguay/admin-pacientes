7400d2962539b29e70f9b3aa4a859c6332318888
martin_dev <gmsuruguay@gmail.com>
2020-10-17 18:35:15 -0300

    se implemento metodo editar

--- src/views/Editar.vue
+++ src/views/Editar.vue
@@ -2,7 +2,27 @@
     <div>
         <Header/>
         <div class="container">
-            Editar
+            <div :class="[status ? 'alert-success' : 'alert-danger', 'alert']" role="alert" v-if="status">
+            {{mensaje}}
+            </div>
+           <form>
+                <div class="form-group">
+                    <label for="inputDni">DNI</label>
+                    <input type="text" class="form-control" id="exampleInputDni" v-model="paciente.dni">
+                    
+                </div>
+                <div class="form-group">
+                    <label for="inputNombre">Nombre</label>
+                    <input type="text" class="form-control" id="inputNombre" v-model="paciente.nombre">
+                </div>
+                <div class="form-group">
+                    <label for="inputTelefono">Teléfono</label>
+                    <input type="text" class="form-control" id="inputTelefono" v-model="paciente.telefono">
+                </div>
+                
+                <button type="button" class="btn btn-primary" @click="editar()">Editar</button>
+                <button type="button" class="btn btn-info" @click="salir()">Salir</button>
+            </form>
         </div>
     </div>
 </template>
@@ -17,28 +37,44 @@
             Header
         },
         data(){
-            return{
-                pacienteId: null,
+            return{    
+                status : false,
+                mensaje : "",           
                 paciente : {
+                    "pacienteId":"",
                     "nombre" : "",
-                    "dni" : "",
-                    "correo":"",
-                    "codigoPostal" :"",
-                    "genero" : "",
+                    "dni" : "",                                    
                     "telefono" : "",
-                    "fechaNacimiento" : "",
-                    "token" : "" , 
-                    "pacienteId" : ""
+                    "token":""             
+                    
                 }
             }
         },
         mounted(){
-            this.pacienteId = this.$route.params.id
-            let endPoint = "http://solodata.es/pacientes?id="+this.pacienteId
+            this.paciente.pacienteId = this.$route.params.id
+            let endPoint = "http://solodata.es/pacientes?id="+this.paciente.pacienteId
             axios.get(endPoint).then(result =>{
-                this.paciente.nombre = result.data[0].Nombre                
+                this.paciente.nombre = result.data[0].Nombre     
+                this.paciente.dni = result.data[0].DNI     
+                this.paciente.telefono = result.data[0].Telefono,
+                this.paciente.token = localStorage.getItem("token")                         
             })
             //console.log(this.pacienteId)
+        },
+        methods:{
+            editar(){
+                axios.put("http://solodata.es/pacientes",this.paciente).then(result =>{
+                   if(result.data.status == "ok"){
+                       this.status=true
+                       this.mensaje = "Updated success"
+                   }else{
+                       this.mensaje = "Error"
+                   }                     
+            })
+            },
+            salir(){
+                this.$router.push("/dashboard")
+            }
         }
     }
 </script>
